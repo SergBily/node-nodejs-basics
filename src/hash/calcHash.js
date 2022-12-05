fada4d9dbe63ff7804e1d9a2a7c1018384f3933f
SergBily <sergBily.dev@gmail.com>
2022-12-05 19:25:47 +0300

    feat: add solutions hash tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,21 @@
+
+import { createHash } from 'crypto';
+import { createReadStream } from "fs";
+
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const input = createReadStream('./files/fileToCalculateHashFor.txt');
+  // hash.setEncoding('hex');
+
+  // const data = input.read();
+  // if (data) hash.update(data);
+  input.on('readable', () => {
+    const data = input.read();
+    hash.update(data);
+
+  })
+  return hash.digest('hex')
 };
+
+console.log(calculateHash());
