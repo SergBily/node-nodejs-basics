f929a2d32ac9506f93dcc8bc414948170243acde
SergBily <sergbily.dev@gmail.com>
2023-06-19 22:52:58 +0300

    feat: solved cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,17 @@
+import { fork } from 'child_process';
+import url  from 'url';
+import { getSourceUrl } from '../libs/libs-absolute-pass.js';
+
+const __filename = 'script.js';
+const folderName = 'files';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToFile = getSourceUrl(__dirname, `${folderName}/${__filename}`);
+    const childProcess = fork(pathToFile, args, { silent: true });
+
+   process.stdin.pipe(childProcess.stdin);
+   childProcess.stdout.pipe(process.stdout);
 };
+
+spawnChildProcess(['1', '2']);
